9082cc0c5cd210ee5cf6ae3fe33ae3ea5a98d585
tima2 <tima2@DESKTOP-I5RG9PA>
2021-01-19 23:22:12 +0300

    Homework1

--- academy/src/by/academy/homework1/Task2.java
+++ academy/src/by/academy/homework1/Task2.java
@@ -8,28 +8,28 @@ public class Task2 {
         switch (dataType) {
             case "int" -> {
                 System.out.println("Введите переменную типа int");
-                int variable = sc.nextInt();
-                System.out.println(variable % 2);
+                int a1 = sc.nextInt();
+                System.out.println(a1 % 2);
             }
             case "double" -> {
                 System.out.println("Введите переменную типа double");
-                double variable = sc.nextDouble();
-                System.out.println(variable * 0.7);
+                double a2 = sc.nextDouble();
+                System.out.println(a2 * 0.7);
             }
             case "float" -> {
                 System.out.println("Введите переменную типа float");
-                float variable = sc.nextFloat();
-                System.out.println(variable * variable);
+                float a3 = sc.nextFloat();
+                System.out.println(a3 * a3);
             }
             case "char" -> {
                 System.out.println("Введите переменную типа char");
-                char variable = sc.next().charAt(0);
-                System.out.println((int) variable);
+                char a4 = sc.next().charAt(0);
+                System.out.println((int) a4);
             }
             case "String" -> {
                 System.out.println("Введите переменную типа String");
-                String variable = sc.next();
-                System.out.println("Hello " + variable);
+                String a5 = sc.next();
+                System.out.println("Hello " + a5);
             }
             default -> System.out.println("Unsupported type");
         }
